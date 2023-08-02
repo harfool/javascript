350046a6a4adc99f08ec8721c67fa702f0780cab
harfool  gujjar <110320932+harfool@users.noreply.github.com>
2023-08-02 16:34:12 +0000

    container config done

---
+++ javascript basic/02variables.js
@@ -0,0 +1,12 @@
+const accountId = 7965
+let accountEmail = "harfoolgujjar63@gmail.com"
+var accountPassword = "961023"
+city = "bhilware"
+console.log("before update ");
+console.table([accountId , accountEmail , accountPassword , city])
+// accountId = 1387; const value not update
+accountEmail="harfool gujjar767@google.com"
+accountPassword = "9911177"
+city = "jaipur"
+console.log("after update");
+console.table([accountId , accountEmail , accountPassword , city])
---
+++ javascript basic/03dataTypes.js
@@ -0,0 +1,21 @@
+// primitive dataTpye 
+let  age = 32 ; // number 
+let myName = "Harfool"  // string
+let isMarried= true;   // boolean
+let no; // undefined
+let nothing = null ; // null
+
+// number => 2 the power 53
+// bigint => use for big int 
+// boolean => treu false 
+// null => standalone value  that means nothing
+// undefined  => it's a variable without any value assigned to it.
+// symbol => unique identifier
+
+
+// object 
+console.log(typeof (age)) // number 
+console.log(typeof (isMarried)) // boolean 
+console.log(typeof "harfool");// string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
