a29ce278507bf091b3bc4ed9b147186da07bff8a
harfool  gujjar <110320932+harfool@users.noreply.github.com>
2023-08-03 03:54:33 +0000

    container config done

---
+++ javascript basic/datatypeSummery.js
@@ -0,0 +1,14 @@
+// primitive data type
+
+// there are 7 primitive datatype
+// string symbol number null undefined BigInt Boolean
+let name = "harfool" //string
+let sybol = Symbol("har") //symbol
+const numberrr = 123456; // number 
+let undef ; // undefined 
+let nulll =null // object
+let isLogIN = true //Boolean 
+let bigNUm = 3546567879n; // BigInt
+console.table([typeof name ,typeof sybol , typeof numberrr ,typeof undef , typeof nulll , typeof isLogIN , typeof bigNUm])
+// referance (non primitive data type)
+// array object function
