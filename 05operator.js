c614426d7bf4748d79b17eb85d7661c4fb958b9e
harfool  gujjar <110320932+harfool@users.noreply.github.com>
2023-08-03 02:20:25 +0000

    container config done

---
+++ 05operator.js
@@ -0,0 +1,28 @@
+// string concate
+let str1 = "Hallo "
+let str2 = "harfool gujjar"
+let str = str1 + str2
+console.log(str);
+
+ //what if we add a number or string
+ let num =5
+ let strig = "6"
+ console.log(num + strig); // 56
+ console.log(strig + num ); //65
+ console.log(strig + num + num ); // 655
+ console.log(num + num + strig) ; // 106
+ console.log(num + strig + num); //565
+
+ //prefix and postfix
+ let  x =  3
+ console.log(x); // 3
+ let y = x++
+console.log("during prefix = ",y);  // 3
+ console.log("after prefex = ",x); // 4
+  
+ //postfix
+ let z=8;
+ console.log(z); //8
+ let b = ++z
+ console.log("during postfix = ",b); //9
+ console.log("after postfix =  ",z); //
