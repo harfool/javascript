25751734893de40d0998976851236ae25dc4ded2
harfool  gujjar <110320932+harfool@users.noreply.github.com>
2023-08-14 14:39:39 +0000

    config done

--- javascript basic 2/03object_part2.js
+++ javascript basic 2/03object_part2.js
@@ -26,4 +26,8 @@ let data = [
     {id:"123", email:"g@gmail.com"},
     {id:"123", email:"g@gmail.com"}
 ]
-data[1].
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
