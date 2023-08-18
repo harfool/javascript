84aab2fa7744a132eb6ed24959e0029387862194
harfool  gujjar <110320932+harfool@users.noreply.github.com>
2023-08-18 07:30:44 +0000

    loop

---
+++ 05_iterations/02_while_and_do_while.js
@@ -0,0 +1,31 @@
+// let num = 0;
+// while (num <=10) {
+//     console.log(`even number ${num}`);
+//     num= num +2
+// }
+
+// array in while loop
+
+// let arr=[5,6,3,45,56,99];
+// let i = 0;
+// while(i < arr.length){
+//     index= arr[i]
+//     console.log(index);
+//     i++
+// }
+
+
+// do while 
+
+// let score = 1;
+// do{
+//     console.log(`score is ${score}`);
+//     score++
+// }while(score<10)
+
+//if condition false do while run at least one time
+let score = 11;
+do{
+    console.log(`score is ${score}`);
+    score++
+}while(score<10)
